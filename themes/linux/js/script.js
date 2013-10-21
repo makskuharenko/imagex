d5e8eee09363d2e6290f6f2f228895870414fe12
Adam Brown <arbrown@gmail.com>
2013-10-21 10:38:38 -0700

    Adding javascript related to the responsive styles

--- themes/linux/js/script.js
+++ themes/linux/js/script.js
@@ -1,34 +1,68 @@
 (function ($) {
  
+  // jPanel Menu settings
+  var jPM = $.jPanelMenu({
+    menu: '.region-first',
+    trigger: '.menu-utility'
+  });
+
+  var jRes = jRespond([
+      {
+          label: 'small',
+          enter: 0,
+          exit: 850
+      },{
+          label: 'large',
+          enter: 850,
+          exit: 10000
+      }
+  ]);
+
+  // register enter and exit functions for multiple breakpoints and functions
+  jRes.addFunc([
+    {
+        breakpoint: 'small',
+        enter: function() {
+          jPM.on();
+        },
+        exit: function() {
+          jPM.off();
+          //var s = skrollr.init();
+        }
+    },{
+        breakpoint: 'large',
+        enter: function() {
+         //var s = skrollr.init();
+        },
+        exit: function() {
+        }
+    }
+  ]);
+
   // When the pane title is clicked. Show the drop down.
-  $('.pane-test-views-panel-pane-5 .pane-title').click(function() {
+  $('.pane-test-views-panel-pane-5').on('click', '.pane-title', function() {
   	$('.pane-menu-menu-most-recent-filter').show();
   });/*.mouseleave(function() {
   	$('.pane-menu-menu-most-recent-filter').hide();
   });*/
 
-  $('.pane-menu-menu-most-recent-filter a').click(function() {
+  $('.pane-menu-menu-most-recent-filter').on('click', 'a', function() {
   	var selectedFilter = $(this).attr('class');
   	var header = $('.filtered-view h2');
 
+    header.text($(this).text());
   	header.removeClass('popular commented shared');
 
   	switch (selectedFilter) {
   	  case 'popular':
   	  	header.addClass('popular');
-        header.text('Most Popular');
   	  	break;
   	  case 'commented':
   	  	header.addClass('commented');
-        header.text('Most Comments');
   	  	break;
   	  case 'shared':
   	  	header.addClass('shared');
-        header.text('Most Shared');
   	  	break;
-      default:
-        header.text('Most Recent');
-        break;
   	}
 
     $(this).parent().prependTo($(this).parents('ul'));
@@ -42,7 +76,6 @@
   var eTop = $('#block-panels-mini-global-header').offset().top; //get the offset top of the element
 
   $(window).scroll(function() { //when window is scrolled
-    console.log(eTop - $(window).scrollTop());
       if (eTop - $(window).scrollTop() <= 0) {
         $('#header-wrapper, #panel-wrapper').addClass('fixed');
       } else {
