439a14606afbeba78207465f0104a81adbcb0a40
Adam Gregory <arcaneadam@gmail.com>
2013-12-03 00:28:06 -0800

    Fixing a errent debug

--- themes/linux/templates/node--blog--sidebar.tpl.php
+++ themes/linux/templates/node--blog--sidebar.tpl.php
@@ -8,7 +8,7 @@
     <?php print l(render($content['field_image']), 'node/'. $nid, array('html' => TRUE)); ?>
   </div>
 </div>
-<span class="views-field views-field-created"><span class="field-content"><?php print $type;?> <?php print l(date('M j', $created), 'node/'. $nid, array('html' => TRUE)); ?></span></span>
+<span class="views-field views-field-created"><span class="field-content"><?php print l(date('M j', $created), 'node/'. $nid, array('html' => TRUE)); ?></span></span>
 <div class="views-field views-field-title">
   <span class="field-content"><?php print l($title, 'node/'. $nid); ?></span>
 </div>
