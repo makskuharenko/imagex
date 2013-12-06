06e49b176ea05f315dd036644ecf693e21cb331d
amcgowanca <me@amcgowan.ca>
2013-12-06 09:34:11 -0700

    Removed uuid.php temp testing file from landing pages.

--- modules/features/linux_landing_pages/uuid.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once 'linux_landing_pages.features.uuid_node.inc';
-$uuids = linux_landing_pages_uuid_features_default_content();
-foreach ($uuids as $node) {
-  echo "// {$node['title']} \n";
-  echo "'{$node['uuid']}' => '',\n";
-}
